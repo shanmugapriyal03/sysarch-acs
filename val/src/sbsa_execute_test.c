ca2a4c6cd1bcefc8d87570dbdd679a216a2087c7
Srikar Josyula <srikar.josyula@arm.com>
2025-06-25 23:51:07 +0530

    refactor(PCIe): split ACS capability test by rule
     - p082 now validates iEP_EP devices for IE_ACS_1
     - New test p015 checks ACS support on RCiEP devices for RE_ACS_1
     - New test p016 reports RE_ACS_2 based on AER capability availability
     - Added logic to track AER capability status globally and report RE_ACS_2 accordingly
    
    Signed-off-by: Srikar Josyula <srikar.josyula@arm.com>
    Change-Id: I9af1992e640187460d17d6f768d3c6a5eed7f945

--- val/src/sbsa_execute_test.c
+++ val/src/sbsa_execute_test.c
@@ -374,6 +374,8 @@ val_sbsa_pcie_execute_tests(uint32_t level, uint32_t num_pe)
     }
 
 #ifndef TARGET_LINUX
+      status |= p015_entry(num_pe);
+      status |= p016_entry(num_pe); /* Depends on p015; run it prior to this test */
       status |= p027_entry(num_pe);
       status |= p028_entry(num_pe);
       status |= p034_entry(num_pe);
