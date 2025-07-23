192d4c2ece62af218cc4ae86b46335ff0fcc653a
Ajayswar S <ajayswar.s@arm.com>
2025-07-24 04:26:40 +0530

    Add PC BSA test for PCIe module
    - Introduced rule P_L1SM_05 under #ifdef
    - Test is skipped if SMMU is not present
    
    Signed-off-by: Ajayswar S <ajayswar.s@arm.com>
    Change-Id: I7713fd9faf1cb5dc99928c3d802dc50129ddd405

--- val/src/pc_bsa_execute_test.c
+++ val/src/pc_bsa_execute_test.c
@@ -177,6 +177,7 @@ val_pcbsa_smmu_execute_tests(uint32_t level, uint32_t num_pe)
       }
 
       status |= i020_entry(num_pe);
+      status |= i022_entry(num_pe);
   }
 
   val_print_test_end(status, "SMMU");
